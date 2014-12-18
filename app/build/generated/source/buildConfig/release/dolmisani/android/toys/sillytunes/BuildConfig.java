4a3d53e3650a31756c53c4c7fa9b82135b11ecca
Daniele Olmisani <daniele.olmisani@gmail.com>
2014-12-18 22:01:29 +0100

    first buildable version

---
+++ app/build/generated/source/buildConfig/release/dolmisani/android/toys/sillytunes/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dolmisani.android.toys.sillytunes;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "dolmisani.android.toys.sillytunes";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
